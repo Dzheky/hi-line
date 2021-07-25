a9aae5913fd9cd5cdc1e90a2da36d7e21d99147c
Evgeny Klimenchenko <Zklimenchenko@gmail.com>
2021-07-25 15:00:15 +0100

    inital commit

---
+++ highlightList.js
@@ -0,0 +1,10 @@
+let highlightList = document.getElementById("highlight-list")
+
+chrome.storage.sync.get("highlights", ({ highlights }) => {
+    if (highlights.length) {
+        highlightList.innerHTML = highlights.map(highlight =>
+            `<a class="highlight-item" target="_blank" href="${highlight.url}">${highlight.selection}</a>`).join("")
+    } else {
+        highlightList.innerHTML = `<div>No highlights found</div>`
+    }
+})
